0c53fb265811c85007ad76deb37b83451fb5c569
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-23 18:25:04 +0700

    Brew formula update for unsavory version v0.2.0

--- unsavory.rb
+++ unsavory.rb
@@ -1,9 +1,9 @@
 class Unsavory < Formula
   desc "Removes outdated Pinboard bookmarks"
   homepage "https://github.com/citizen428/unsavory"
-  url "https://github.com/citizen428/unsavory/releases/download/v0.1.0/unsavory_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "163deb3d93c5b766d0a24bf55827f2389944e09a1e233a45b8b194dcfcd17142"
+  url "https://github.com/citizen428/unsavory/releases/download/v0.2.0/unsavory_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "61004a822e71eaf96c64183f47568a2ab619be03cd0176c9b34275a55cd9f989"
 
   def install
     bin.install "unsavory"
